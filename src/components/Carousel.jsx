ad6a4cd50f66f913e4492066a48dd26fd921c6b1
Agastya Singh <122881171+ugh-astya@users.noreply.github.com>
2023-06-06 13:09:05 +0530

    .jpg to .webp changes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,16 +8,16 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 // import images
-import img1 from "../assets/carousel/1.jpg";
-import img2 from "../assets/carousel/2.jpg";
-import img3 from "../assets/carousel/3.jpg";
-import img4 from "../assets/carousel/4.jpg";
-import img5 from "../assets/carousel/5.jpg";
-import img6 from "../assets/carousel/6.jpg";
-import img7 from "../assets/carousel/7.jpg";
-import img8 from "../assets/carousel/8.jpg";
-import img9 from "../assets/carousel/9.jpg";
-import img10 from "../assets/carousel/10.jpg";
+import img1 from "../assets/carousel/1.webp";
+import img2 from "../assets/carousel/2.webp";
+import img3 from "../assets/carousel/3.webp";
+import img4 from "../assets/carousel/4.webp";
+import img5 from "../assets/carousel/5.webp";
+import img6 from "../assets/carousel/6.webp";
+import img7 from "../assets/carousel/7.webp";
+import img8 from "../assets/carousel/8.webp";
+import img9 from "../assets/carousel/9.webp";
+import img10 from "../assets/carousel/10.webp";
 
 const Carousel = () => {
     const imgArray = [
